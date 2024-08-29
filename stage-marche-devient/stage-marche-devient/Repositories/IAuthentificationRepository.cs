2d92cac3a0552495f45d51329ec8d7c8d45d1da0
CestPasFo <c.jaworski12@hotmail.fr>
2024-08-29 15:05:10 +0200

    Ajouts des méthodes de connexion et d'authentification avec hashage du mot de passe avec salt+pepper

---
+++ stage-marche-devient/stage-marche-devient/Repositories/IAuthentificationRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace stage_marche_devient.Repositorys
+{
+    public interface IAuthentificationRepository
+    {
+            Utilisateur Connexion(string mail, string password);
+            void InscriptionUtilisateur(Utilisateur user);
+    }
+}
