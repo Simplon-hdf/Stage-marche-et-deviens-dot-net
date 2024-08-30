c7f8a01975362899b0f9acdef12910b7638d73c8
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-30 12:23:29 +0200

    feat(card-sejours): Ajout du composant card sejours

---
+++ stage-marche-devient-front/src/app/Components/card-sejour/card-sejour.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-sejour',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-sejour.component.html',
+  styleUrl: './card-sejour.component.scss'
+})
+export class CardSejourComponent {
+
+}
