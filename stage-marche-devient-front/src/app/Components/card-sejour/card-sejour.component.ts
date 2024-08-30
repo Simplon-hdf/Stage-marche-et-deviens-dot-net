aa04caf5176243510e973ed3b63d706bd417e4a3
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-30 15:13:53 +0200

    feat(card-sejours): Initialisation des function de recuperation de donnée de la bdd

--- stage-marche-devient-front/src/app/Components/card-sejour/card-sejour.component.ts
+++ stage-marche-devient-front/src/app/Components/card-sejour/card-sejour.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RandonneeService} from "../../Services/randonnee.service";
+import {Randonnee} from "../../Models/randonnee.model";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-card-sejour',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './card-sejour.component.html',
   styleUrl: './card-sejour.component.scss'
 })
-export class CardSejourComponent {
+export class CardSejourComponent implements OnInit{
+  randos: Randonnee[] = [];
+  error: string = " ";
 
+  constructor(private randonneeService: RandonneeService) {}
+
+  ngOnInit(): void {
+    this.loadRandonnee();
+  }
+
+  loadRandonnee(): void {
+    this.randonneeService.getRandos().subscribe({
+      next: (data) => {
+        this.randos = data;
+        console.log('Randonnées chargées:', this.randos);
+      },
+      error: (err) => {
+        this.error = 'Erreur lors du chargement des randonnées';
+        console.error('Erreur:', err);
+      }
+    });
+  }
 }
