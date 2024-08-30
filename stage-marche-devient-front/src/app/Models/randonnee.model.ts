43be10644fea2170c4301f0dbb67df0391a232a8
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-08-30 14:13:07 +0200

    feat(randonnee.model): Création du model de randonnee

---
+++ stage-marche-devient-front/src/app/Models/randonnee.model.ts
@@ -0,0 +1,13 @@
+export class Randonnee {
+  id?: number;
+  nom?: string;
+  desc?: string;
+  lieu?: string;
+  image?: string;
+  prix?: number;
+  nbr_nuit?: number;
+  min_parti?: number;
+  max_parti?: number;
+  visible_font?: number;
+  distance_kms?: number;
+}
