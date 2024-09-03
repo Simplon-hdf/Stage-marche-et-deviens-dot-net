13cd9a43cd334f645b26ff6d041fe477bc6fc004
SamiraCemoi <samiracommeca@gmail.com>
2024-09-03 09:57:47 +0200

    feat : creation config app.module.ts et route.ts

---
+++ stage-marche-devient-front/src/app/concept/concept.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConceptComponent } from './concept.component';
+
+describe('ConceptComponent', () => {
+  let component: ConceptComponent;
+  let fixture: ComponentFixture<ConceptComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ConceptComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConceptComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
