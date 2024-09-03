10d5fbc8dfc57962229b8240be7d0516d23d6f22
CestPasFo <c.jaworski12@hotmail.fr>
2024-09-03 10:31:52 +0200

    Feat: Création de la page dynamique de détail des randonnées

---
+++ stage-marche-devient-front/src/app/detail-offre/detail-offre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-offre',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-offre.component.html',
+  styleUrl: './detail-offre.component.scss'
+})
+export class DetailOffreComponent {
+
+}
