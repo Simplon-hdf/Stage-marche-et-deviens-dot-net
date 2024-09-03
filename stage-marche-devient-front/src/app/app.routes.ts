358478e13ae055d8883f22a56333535cc632530a
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-09-03 11:06:02 +0200

    fix(routes, main.ts): Resolution probleme de route et main.ts

--- stage-marche-devient-front/src/app/app.routes.ts
+++ stage-marche-devient-front/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CardSejourComponent } from './Components/card-sejour/card-sejour.component';
+import { SejourComponent } from './Components/sejour/sejour.component';
 
 export const routes: Routes = [
     { path: 'home', component: AppComponent},
-    { path: 'sejours', component: CardSejourComponent},
+    { path: 'sejours', component: SejourComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
