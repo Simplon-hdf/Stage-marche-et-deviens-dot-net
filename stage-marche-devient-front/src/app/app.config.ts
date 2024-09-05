66d08e12ce3fa924fb30e3c002267c6ca236895e
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-09-05 16:29:25 +0200

    fix!: Gestion des imports

--- stage-marche-devient-front/src/app/app.config.ts
+++ stage-marche-devient-front/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
--- stage-marche-devient-front/src/app/app.module.ts
+++ stage-marche-devient-front/src/app/app.module.ts
@@ -1,28 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app.routes';
-import { AppComponent } from './app.component'; 
-import { ConceptComponent } from './concept/concept.component';
-import { provideHttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './Components/navbar/navbar.component';
-import { SejourComponent } from './Components/sejour/sejour.component';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app.routes";
+import { ConceptComponent } from "./Pages/concept/concept.component";
+import { provideHttpClient } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { SejourComponent } from "./Components/sejour/sejour.component";
+import { HomeComponent } from "./Pages/home/home.component";
 
 @NgModule({
   declarations: [
     // Ajoutez d'autres composants ici
   ],
   imports: [
-   
+    HomeComponent,
     ConceptComponent,
     NavbarComponent,
     CommonModule,
     SejourComponent,
     BrowserModule,
-    AppRoutingModule // Importation du module de routage
+    AppRoutingModule, // Importation du module de routage
   ],
   providers: [provideHttpClient()],
-  bootstrap: [] // Définir le composant racine
+  bootstrap: [], // Définir le composant racine
 })
-export class AppModule { }
+export class AppModule {}
