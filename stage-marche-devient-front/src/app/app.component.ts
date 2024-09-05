c2bb9ab01069f82f859b6ac19dcfb2e1219b5493
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-09-05 16:30:32 +0200

    fix(): Gestion des import de app.component.ts

--- stage-marche-devient-front/src/app/app.component.ts
+++ stage-marche-devient-front/src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from "./Components/navbar/navbar.component";
-import { SejourComponent } from './Components/sejour/sejour.component';
-import { ConceptComponent } from './concept/concept.component'; 
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { SejourComponent } from "./Components/sejour/sejour.component";
+import { ConceptComponent } from "./Pages/concept/concept.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-    imports: [RouterOutlet, NavbarComponent, SejourComponent, ConceptComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NavbarComponent, SejourComponent, ConceptComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'stage-marche-devient-front';
+  title = "stage-marche-devient-front";
 }
