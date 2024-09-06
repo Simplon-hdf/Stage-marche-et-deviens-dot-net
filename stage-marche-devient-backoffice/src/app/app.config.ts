8af2585ac334e50b09e1d71e659a1b533b57fcdf
Renaud Baussart <132342520+RenaudBaussart@users.noreply.github.com>
2024-09-06 11:49:32 +0200

    feat: ajout / supression / visualisation list composant randonnee

--- stage-marche-devient-backoffice/src/app/app.config.ts
+++ stage-marche-devient-backoffice/src/app/app.config.ts
@@ -4,7 +4,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { of } from 'rxjs';
+import { NgModel } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration() , provideHttpClient() , CommonModule]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration() , provideHttpClient() , CommonModule,NgModel]
 };
