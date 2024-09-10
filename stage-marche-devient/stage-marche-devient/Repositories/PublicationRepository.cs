214ac83e62fcb247025ac9d295efa6b124d0c6b0
fat5B <myshop.ino@gmail.com>
2024-09-10 13:47:32 +0200

    modification methode delete

--- stage-marche-devient/stage-marche-devient/Repositories/PublicationRepository.cs
+++ stage-marche-devient/stage-marche-devient/Repositories/PublicationRepository.cs
@@ -46,14 +46,23 @@ namespace stage_marche_devient.Repositories
 
         public async Task<bool> Update(Publication model, int id)                            //Fonction de mise-à-jour d'une publication dans la base de données
         {
-            var dbRandonnee = await _context.Publication.FindAsync(id);                      //On récupère l'Id de la publication à laquelle on souhaite apporter des modifications
-            dbRandonnee.NomPublication = model.NomPublication;                               //On change la valeur du nom de la publication par celle rentrée par l'utilisateur
-            dbRandonnee.LienMedia = model.LienMedia;                                         //On change la valeur du chemin d'accès à l'image de la publication par celle rentrée par l'utilisateur
-            dbRandonnee.ContenuTexte = model.ContenuTexte;                                   //On change le contenu de la publictaion par celle rentrée par l'utilisateur
-            dbRandonnee.IdSession = model.IdSession;                                         //On change l'Id de la session associé à la publication par celle rentrée par l'utilisateur
+            var dbPublication = await _context.Publication.FindAsync(id);                      //On récupère l'Id de la publication à laquelle on souhaite apporter des modifications
+            if (dbPublication== null)
+            {
+                return false;
+            }
+
+            dbPublication.NomPublication = model.NomPublication;                               //On change la valeur du nom de la publication par celle rentrée par l'utilisateur
+            dbPublication.LienMedia = model.LienMedia;                                         //On change la valeur du chemin d'accès à l'image de la publication par celle rentrée par l'utilisateur
+            dbPublication.ContenuTexte = model.ContenuTexte;                                   //On change le contenu de la publictaion par celle rentrée par l'utilisateur
+            dbPublication.IdSession = model.IdSession;                                         //On change l'Id de la session associé à la publication par celle rentrée par l'utilisateur
+            
             await _context.SaveChangesAsync();                                               //On sauvegarde les changements apportés à la base de données   
             var dbVerifAction = await _context.Publication.FindAsync(id);
-            return dbVerifAction.NomPublication == model.NomPublication &&                   //On verifie les changements apportés par l'utilisateur
+
+            return dbVerifAction != null &&
+
+                    dbVerifAction.NomPublication == model.NomPublication &&                   //On verifie les changements apportés par l'utilisateur
                     dbVerifAction.LienMedia == model.LienMedia &&
                     dbVerifAction.ContenuTexte == model.ContenuTexte &&
                     dbVerifAction.IdSession == model.IdSession;
