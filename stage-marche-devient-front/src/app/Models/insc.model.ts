9e7f12091b35d9a8dfd300f63b958baddb36703d
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-09-10 11:31:30 +0200

    feat():model d'inscription

---
+++ stage-marche-devient-front/src/app/Models/insc.model.ts
@@ -0,0 +1,8 @@
+export class UserRegister {
+    nomUtilisateur?: string;
+    prenomUtilisateur?: string;
+    mailUtilisateur?: string;
+    telUtilisateur?: string;
+    mdpUtilisateur?: string;
+    totalDistanceParcourueUtilisateur: number = 0;
+}
