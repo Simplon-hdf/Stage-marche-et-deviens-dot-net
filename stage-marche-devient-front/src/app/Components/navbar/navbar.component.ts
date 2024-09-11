610e1d8512f610433b8801676fd607932afa7868
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-09-11 16:06:15 +0200

    feat(): Ajout des "controlle" de connexion

--- stage-marche-devient-front/src/app/Components/navbar/navbar.component.ts
+++ stage-marche-devient-front/src/app/Components/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ConnexionService } from '../../Services/connexion.service';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterModule],
-
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  conService = inject(ConnexionService);
 }
