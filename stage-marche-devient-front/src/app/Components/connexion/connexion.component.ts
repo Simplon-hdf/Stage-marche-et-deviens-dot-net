b574e469b61ddbd75794252cf13798d13962ae51
MathLefb <146764381+wazeeeeee@users.noreply.github.com>
2024-09-11 12:50:54 +0200

    feat(connexion): Création du component de connexion utilisateur

---
+++ stage-marche-devient-front/src/app/Components/connexion/connexion.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, OnDestroy } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { ConnexionService } from "../../Services/connexion.service";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { UserCredentials } from "../../Models/con.model";
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: "app-connexion",
+  standalone: true,
+  imports: [ReactiveFormsModule, MatButtonModule, MatInputModule],
+  templateUrl: "./connexion.component.html",
+  styleUrl: "./connexion.component.scss",
+})
+export class ConnexionComponent implements OnDestroy {
+  private formBuilder = inject(FormBuilder);
+  private loginService = inject(ConnexionService);
+  private router = inject(Router);
+  private loginSubscription: Subscription | null = null;
+
+  loginFormGroup = this.formBuilder.group({
+    mailUtilisateur: ["", [Validators.required]],
+    motDePasse: ["", [Validators.required]],
+  });
+  invalidCredentials = false;
+
+  login() {
+    this.loginSubscription = this.loginService
+      .login(this.loginFormGroup.value as UserCredentials)
+      .subscribe({
+        next: (result) => {
+          this.router.navigate(["home"]);
+        },
+        error: (error) => {
+          this.invalidCredentials = true;
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.loginSubscription?.unsubscribe();
+  }
+}
