4c66b7ab23bdb5ada23ba0950e25415a408225dc
fat5B <myshop.ino@gmail.com>
2024-09-11 11:19:51 +0200

    fix:modification methode PUT et Delete

--- stage-marche-devient/stage-marche-devient/Repositories/PublicationRepository.cs
+++ stage-marche-devient/stage-marche-devient/Repositories/PublicationRepository.cs
@@ -38,10 +38,15 @@ namespace stage_marche_devient.Repositories
         public async Task<bool> Delete(int id)                                               //Fonction de suppression d'une publication à la base de données
         {
             var bddPublicationSupprimer = await _context.Publication.FindAsync(id);          //On recupère l'Id de la publication qu'on souhaite supprimer
-            if (bddPublicationSupprimer == null) { return false; }                           //Si la publication n'existe pas on retourne une erreur
+           
+            if (bddPublicationSupprimer == null) 
+            {
+                return false; 
+            }                                                                                 //Si la publication n'existe pas on retourne une erreur
             _context.Publication.Remove(bddPublicationSupprimer);                            //Sinon on supprime l'entité de la base de donnée
             await _context.SaveChangesAsync();                                               //On sauvegarde les changements apportés à la base de données     
-            return await _context.Randonnee.FindAsync(id) != null;                           //On verifie que l'ajout a bien été réalisé
+            
+            return true;                                                                        //On verifie que l'ajout a bien été réalisé
         }
 
         public async Task<bool> Update(Publication model, int id)                            //Fonction de mise-à-jour d'une publication dans la base de données
