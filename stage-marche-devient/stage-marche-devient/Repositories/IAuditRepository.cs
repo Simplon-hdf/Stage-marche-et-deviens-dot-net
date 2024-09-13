615cfcf6997c556247738b92ac7b6ae94dbedcf0
CestPasFo <c.jaworski12@hotmail.fr>
2024-09-13 14:10:27 +0200

    Feat: Creation d'une interface pour la gestion des logs

---
+++ stage-marche-devient/stage-marche-devient/Repositories/IAuditRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace stage_marche_devient.Repositories
+{
+    public interface IAuditRepository
+    {
+        Task LogAsync(string userName, string action, string entityName, string details = null);
+    }
+}
