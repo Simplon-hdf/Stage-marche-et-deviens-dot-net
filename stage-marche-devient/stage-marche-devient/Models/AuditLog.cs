dad5fdd1a0f34a6f3d3aac44680dab09efb07c38
CestPasFo <c.jaworski12@hotmail.fr>
2024-09-13 13:58:10 +0200

    Feat: Création du model des logs

---
+++ stage-marche-devient/stage-marche-devient/Models/AuditLog.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace stage_marche_devient.Models
+{
+    public class AuditLog
+    {
+        [Column("id_log"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
+        public int Id { get; set; }
+        [Column("prenom_utilisateur"), Required]
+        public string Utilisateur { get; set; } // L'utilisateur qui a effectué l'action
+        [Column("prenom_utilisateur"), Required]
+        public string Action { get; set; } // L'action exécutée (ex: "create", "update", "delete")
+        [Column("prenom_utilisateur"), Required]
+        public string EntityName { get; set; } // L'entité concernée (ex: "Order", "User")
+        [Column("prenom_utilisateur"), Required]
+        public string DateEvenement { get; set; } // La date et l'heure de l'action
+        [Column("prenom_utilisateur"), Required]
+        public string Details { get; set; } // Détails additionnels (optionnel)
+    }
+}
