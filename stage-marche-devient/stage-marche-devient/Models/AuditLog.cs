58c8deb350ee60626c69e8cbd8a73224b5b2ca09
CestPasFo <c.jaworski12@hotmail.fr>
2024-09-13 14:05:01 +0200

    Fix: Modification nom collone pour match bdd

--- stage-marche-devient/stage-marche-devient/Models/AuditLog.cs
+++ stage-marche-devient/stage-marche-devient/Models/AuditLog.cs
@@ -5,17 +5,17 @@ namespace stage_marche_devient.Models
 {
     public class AuditLog
     {
-        [Column("id_log"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
+        [Column("id_evenement"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
         public int Id { get; set; }
-        [Column("prenom_utilisateur"), Required]
+        [Column("nom_utilisateur"), Required]
         public string Utilisateur { get; set; } // L'utilisateur qui a effectué l'action
-        [Column("prenom_utilisateur"), Required]
+        [Column("action_realise"), Required]
         public string Action { get; set; } // L'action exécutée (ex: "create", "update", "delete")
-        [Column("prenom_utilisateur"), Required]
+        [Column("cible_action"), Required]
         public string EntityName { get; set; } // L'entité concernée (ex: "Order", "User")
-        [Column("prenom_utilisateur"), Required]
+        [Column("date_evenement"), Required]
         public string DateEvenement { get; set; } // La date et l'heure de l'action
-        [Column("prenom_utilisateur"), Required]
+        [Column("details_evenement"), Required]
         public string Details { get; set; } // Détails additionnels (optionnel)
     }
 }
