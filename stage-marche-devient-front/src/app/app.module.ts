8553d1dfa9eb09e4d45def3b0f0ae7884ad97997
SamiraCemoi <samiracommeca@gmail.com>
2024-09-16 12:04:26 +0200

    fix: resolution problèmes de config routing et module

--- stage-marche-devient-front/src/app/app.module.ts
+++ stage-marche-devient-front/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutesModule } from './app.routes';
+//import { AppRoutesModule } from './app.routes'; // configuration avec des composants autonomes (standalone) et bootstrapApplication donc plus besoin d'importer AppRoutesModule 
 import { AppComponent } from './app.component'; 
 import { ConceptComponent } from './concept/concept.component';
 import { provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './Components/navbar/navbar.component';
-// import { CardSejourComponent  } from './Components/card-sejour/card-sejour.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './Services/jwt.interceptor';
 import { SejourComponent  } from './Components/sejour/sejour.component';
@@ -31,9 +30,9 @@ import { CsrfInterceptor } from './Services/csrf-interceptor';
     DetailOffreComponent,
     SejourComponent,
     BrowserModule,
-    AppRoutesModule // Importation du module de routage
+    
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }, provideHttpClient(), { provide: HTTP_INTERCEPTORS, useClass: CsrfInterceptor, multi: true }],
-  bootstrap: [AppComponent] // Définir le composant racine
+  
 })
 export class AppModule { }
