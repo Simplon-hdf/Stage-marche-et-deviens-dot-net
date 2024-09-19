d0d7a3dba8949735fd82273e967cbe14517e9277
CestPasFo <c.jaworski12@hotmail.fr>
2024-09-19 13:50:12 +0200

    Fix : Ajout logger randonnee

--- stage-marche-devient/stage-marche-devient/Controllers/RandonneeController.cs
+++ stage-marche-devient/stage-marche-devient/Controllers/RandonneeController.cs
@@ -18,8 +18,8 @@ namespace stage_marche_devient.Controllers
         public RandonneeController(ApiDbContext context, IAuditRepository<AuditLog> auditRepository, ILogger<RandonneeController> logger, ILogger<RandonneeRepository> randonneeLogger)
         {
             _context = context;
-            _repository = new RandonneeRepository(_context);
             _logger = logger;
+            _repository = new RandonneeRepository(_context, randonneeLogger);
             _auditRepository = auditRepository;
 
         }
