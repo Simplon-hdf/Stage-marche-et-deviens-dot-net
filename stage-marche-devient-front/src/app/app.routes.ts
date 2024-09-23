e51fcb07656df6f5e7fd09d08bc01510e4b661f9
SamiraCemoi <samiracommeca@gmail.com>
2024-09-23 11:42:07 +0200

    fix conflicts app.routes et maj details offre css

--- stage-marche-devient-front/src/app/app.routes.ts
+++ stage-marche-devient-front/src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { DetailOffreComponent } from './detail-offre/detail-offre.component';
 import { PageDevisComponent } from './page-devis/page-devis.component';
 import { ConnexionComponent } from './Components/connexion/connexion.component';
 import { InscriptionComponent } from './Components/inscription/inscription.component';
+import { ReservationComponent } from './Components/reservation/reservation.component';
 import { IndexFrontPageComponent } from './Pages/index-front-page/index-front-page.component';
+
 
 
 
@@ -15,7 +17,8 @@ export const routes: Routes = [
   { path: 'concept', component: ConceptComponent },
   { path: 'randonnee/:id', component : DetailOffreComponent},
   { path: 'connexion', component: ConnexionComponent },
-  { path: 'inscription', component: InscriptionComponent }
+  { path: 'inscription', component: InscriptionComponent },
+  { path : 'boutonInscription', component: ReservationComponent }
   
 ];
 
